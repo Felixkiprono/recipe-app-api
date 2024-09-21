58db12130231906a872a90e75bd424e877185cd4
kipronovich <felixkiptor@gmail.com>
2024-09-21 13:11:14 +0300

    configured postgresql,add wait_db check,added tests  for waiting db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,12 +1,9 @@
 """
 Calculator function
 """
-
 def add(x,y):
     """add x and y"""
     return x+y
-
-
 
 def subtract(x,y):
     """minus x and y"""
--- app/app/test.py
+++ app/app/test.py
@@ -1,23 +1,15 @@
 """""
 Sample tests
 """
-
 from django.test import SimpleTestCase
-
 from app import calc
-
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
-
-    def test_add_numbers(self):
-        """"Test adding of two nimnbers"""
-        res = calc.add(5,4)
-
-        self.assertEqual(res,9)
-    
-
-    def test_subtract_numbers(self):
-        """Test subtract numbers."""
-
-        res = calc.subtract(5,3)
-        self.assertEqual(res,2)
+        """Test the calc module."""
+        def test_add_numbers(self):
+            """"Test adding of two nimnbers"""
+            res = calc.add(5, 4)
+            self.assertEqual(res, 9)
+        def test_subtract_numbers(self):
+            """Test subtract numbers."""
+            res = calc.subtract(5, 3)
+            self.assertEqual(res, 2)
